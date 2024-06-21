47e254b6b8ebbb83ed0638daec9665fa67374a03
kalilsub <khalilsubaan@gmail.com>
2024-06-21 21:22:22 +0200

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,20 @@
+<script>
+  import "./app.css"
+
+  import Question from "./lib/Question.svelte"
+</script>
+
+<main>
+  <Question number={1} question="Do you like Svelte?" />
+  <Question number={2} question="Do you like Tailwind CSS?" />
+  <Question number={3} question="Do you like Snowpack?" />
+  <Question number={4} question="Do you like Vite?" />
+  <Question
+    number={5}
+    question="Do you like the combination of Svelte, Tailwind CSS, Snowpack, and Vite?"
+  />
+  <Question
+    number={6}
+    question="Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis, odio?"
+  />
+</main>
---
+++ src/lib/Question.svelte
@@ -0,0 +1,38 @@
+<script>
+  export let question
+  export let number
+
+  let selectedAnswer
+
+  const answers = [
+    { label: "Yes", value: "yes" },
+    { label: "Rather yes", value: "rather_yes" },
+    { label: "Rather no", value: "rather_no" },
+    { label: "No", value: "no" },
+  ]
+
+  function selectAnswer(value) {
+    selectedAnswer = value
+  }
+</script>
+
+<div class=" my-2.5 max-w-xl bg-stone-800 px-8 py-6 text-zinc-50">
+  <div class=" mb-6 text-lg">
+    {number}. {question}
+  </div>
+  <div class="flex justify-center">
+    {#each answers as answer, index}
+      <!-- svelte-ignore a11y-click-events-have-key-events -->
+
+      <button
+        class="ml-1 w-20 cursor-pointer px-2 py-1
+          {selectedAnswer === answer.value ? 'bg-stone-400' : 'bg-stone-700'}
+          {index === 0 ? 'rounded-l-xl' : ''}
+        {index === answers.length - 1 ? 'rounded-r-xl' : ''}"
+        on:click={() => selectAnswer(answer.value)}
+      >
+        {answer.label}
+      </button>
+    {/each}
+  </div>
+</div>
